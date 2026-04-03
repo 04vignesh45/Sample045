f4e46e0ca1e93a3ed4596018fe596ffc9c89d19c
04vignesh45 <vignesh04b01@gmail.com>
2026-04-03 12:36:04 +0530

    added

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your name: ")
-	name, _ := reader.ReadString('\n')
+	name := "John"
+
 	fmt.Println("Hello, " + name)
 }
